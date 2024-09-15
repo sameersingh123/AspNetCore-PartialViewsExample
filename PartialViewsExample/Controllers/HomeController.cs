4c8e6b6ba4e85bb09cb8f932a998907c89f908b9
sameer singh <sameer8182singh@gmail.com>
2024-09-15 14:54:40 +0530

    Add project files.

---
+++ PartialViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewsExample.Models;
+
+namespace PartialViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("programming-languages")]
+        public IActionResult ProgrammingLanguages()
+        {
+            ListModel listModel = new ListModel()
+            {
+                ListTitle = "Programming languages List",
+                ListItems = new List<string>()
+                {
+                    "C#",
+                    "Lang",
+                    "Go",
+                    "Java"
+                }
+            };
+           
+            
+            return PartialView("_ListPartialView",listModel);
+        }
+    }
+}
